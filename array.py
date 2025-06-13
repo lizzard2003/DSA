c0d303ac715a4588f2e15a7eaea1853586f18d96
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2025-06-12 21:20:00 -0500

    second with comments

--- array.py
+++ array.py
@@ -10,13 +10,15 @@ from typing import List
 def removeDuplicates(nums: List[int]) -> int:
 
     l = 1  # left starts at 1
-    for r in range(1, len(nums)):
-        if nums[r] != nums[r - 1]:
-            nums[l] = nums[r]
-            l += 1
-    return l
+    for r in range(
+        1, len(nums)
+    ):  # for r in range starting at 1 and going throughtout the length of nums
+        if nums[r] != nums[r - 1]:  # if the right does not equal to r-1
+            nums[l] = nums[r]  # then l and right are equal
+            l += 1  # then increment the left pointer by 1
+    return l  # return all the numbers that are left
 
 
-nums = [1, 1, 2]
+nums = [1, 1, 2, 3, 5, 5]
 lenght = removeDuplicates(nums)
 print(nums[:lenght])
